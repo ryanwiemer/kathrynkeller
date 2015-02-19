8e2d44b3d8df8dce4dd857f4b32429c9d37ef423
Ryan Wiemer <ryan@ryanwiemer.com>
2015-02-19 01:08:50 -0800

    working on 100% height

--- pages/Home.php
+++ pages/Home.php
@@ -4,7 +4,7 @@
 * Description: A Page Template for the Home Page
 */
 get_header(); ?>
-
+<!--
 <main class="site-content">
 <div class="carousel">
 	<?php if(get_field('carousel')): ?>
@@ -28,6 +28,27 @@ get_header(); ?>
 		</div>
 	<?php endwhile; ?>
 	<?php endif; ?>
+</div>
+</main>
+
+-->
+
+
+<main class="site-content">
+<div class="carousel">
+		<div class="carousel__container">
+			<div class="carousel__image">
+				<img src="http://placekitten.com/g/200/300" />
+			</div>
+			<div class="carousel__details">
+				<a class="carousel__title" href="#">
+					<h2>Lorem Ipsum</h2>
+				</a>
+				<div class="carousel__description">
+					Phasellus dapibus diam sapien, quis ullamcorper nisl commodo ut. Aenean et nisi hendrerit, tincidunt ligula pharetra, condimentum leo. Duis scelerisque mauris vel urna blandit, id luctus sem tristique. Morbi laoreet felis eget nibh egestas vulputate. Proin a dignissim eros. Aenean suscipit nisi ante, at viverra nibh ornare a. Nam sed augue tincidunt, tristique massa eget, semper ex. Nulla auctor ullamcorper mauris. Vestibulum mollis volutpat lacus at aliquet.
+				</div>
+			</div>
+		</div>
 </div>
 </main>
 

3af2441766a43f4347311f74e92a2d6c0395337e
Ryan Wiemer <ryan@ryanwiemer.com>
2015-03-06 14:41:52 -0800

    More homepage updates

--- assets/js/scripts/slick--settings.js
+++ assets/js/scripts/slick--settings.js
@@ -4,11 +4,12 @@ $(document).ready(function(){
   $('.carousel').slick({
     infinite: true,
     autoplay: true,
+    arrows: false,
     autoplaySpeed: 5000,
     speed: 300,
     cssEase: 'linear',
     slidesToShow: 1,
-    fade: true,
+    //fade: true,
     dots: true,
     dotsClass: 'custom_paging',
     customPaging: function (slider, i) {

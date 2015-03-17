c6f453ef0007b7ab565e587e72805918172f0d30
Ryan Wiemer <ryan@ryanwiemer.com>
2015-03-16 17:11:50 -0700

    Working on the About Page

--- pages/About.php
+++ pages/About.php
@@ -6,13 +6,25 @@
 get_header(); ?>
 
 <main class="site-content">
+	<section class="page-header">
+		<h2 class="page-title">About</h2>
+	</section>
+	
+	<div class="about">
+		<?php $large = wp_get_attachment_image_src(get_field('about_image'), 'large'); ?>
+		<?php $medium = wp_get_attachment_image_src(get_field('about_image'), 'medium'); ?>
+		<?php $thumb = wp_get_attachment_image_src(get_field('about_image'), 'thumbnail'); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'page' ); ?>
+			<img class="about__image" srcset="<?php echo $thumbnail[0]; ?> 375w, <?php echo $medium[0]; ?> 750w, <?php echo $large[0]; ?> 1500w">
+			<div class="about__info">
+				<?php the_field('about_info'); ?>
+			</div>
 
 		<?php endwhile; // end of the loop. ?>
 
+	</div>
 </main>
 
 <?php get_footer(); ?>

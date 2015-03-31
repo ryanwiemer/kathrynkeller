99a0501a5d330040e9ab088cbf9727685059233f
Ryan Wiemer <ryan@ryanwiemer.com>
2015-03-31 12:39:25 -0700

    minor update to title tag

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon" />
-<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+<title><?php wp_title('|', true, 'right'); ?></title>
 <!--Typekit-->
 <!--Google Analytics-->
 <?php wp_head(); ?>

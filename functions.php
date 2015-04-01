3033e0d41b305f557ad0f2036a1f326d6882e899
Ryan Wiemer <ryan@ryanwiemer.com>
2015-03-31 19:46:44 -0700

    z-index fixes and modernizr

--- functions.php
+++ functions.php
@@ -163,7 +163,7 @@ function exclude_specific_cats( $wp_query ) {
 //Enqueue scripts and styles.
 function global_scripts() {
   wp_enqueue_style( 'style',  get_stylesheet_directory_uri() . '/assets/css/style.min.css');
-  //wp_enqueue_script( 'modernizr',  get_template_directory_uri() . '/assets/js/vendor/modernizr.min.js');
+  wp_enqueue_script( 'modernizr',  get_template_directory_uri() . '/assets/js/vendor/modernizr.min.js');
   wp_enqueue_script( 'global',  get_template_directory_uri() . '/assets/js/global.min.js', '', '', true);
 }
 

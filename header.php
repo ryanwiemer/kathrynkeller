47bdf824ba490be4d83f7fcc190ffbd8382134f3
Ryan Wiemer <ryan@ryanwiemer.com>
2015-04-06 16:59:00 -0700

    IE fixes

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon" />
 <title><?php wp_title('|', true, 'right'); ?></title>
 <!--Typekit-->

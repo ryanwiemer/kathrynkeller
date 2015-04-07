a1295f797aa914e12e08ca570b63d76d4ea223ac
Ryan Wiemer <ryan@ryanwiemer.com>
2015-04-06 23:07:18 -0700

    minor updates to footer and about page

--- footer.php
+++ footer.php
@@ -7,7 +7,8 @@
 <footer class="site-footer">
   <div class="site-footer__container">
     <div class="site-footer__copyright">&copy; <?php echo date("Y"); ?> Kathryn Keller</div>
-    <a href="#" class="site-footer__facebook">Facebook</a>
+    <?php $facebook_profile = get_the_author_meta( 'user_url' );?>
+    <a href="<?php echo $facebook_profile;?>" class="site-footer__facebook">Facebook</a>
   </div>
 </footer>
 

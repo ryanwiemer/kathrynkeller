54d35a92f14aa797968276dc4df9149ce6d20bba
Ryan Wiemer <ryan@ryanwiemer.com>
2015-04-29 21:44:50 -0700

    Adding Google Analytics

--- header.php
+++ header.php
@@ -5,13 +5,19 @@
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600' rel='stylesheet' type='text/css'>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon" />
 <title><?php wp_title('|', true, 'right'); ?></title>
-<!--Typekit-->
-<!--Google Analytics-->
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-62455441-1', 'auto');
+	ga('send', 'pageview');
+</script>
 <?php wp_head(); ?>
 </head>
 
